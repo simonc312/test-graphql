df84bc07a5efbae6cb1375f76311fe3a92233f11
simonc312 <simon.chen@scalablepress.com>
2018-05-17 18:10:43 -0700

    experiment with type descriptions and directives

--- src/graphql/typedefs/user.ts
+++ src/graphql/typedefs/user.ts
@@ -1,9 +1,19 @@
 export const userTypeDefs: string = `
+  #Someone who can place orders on SP
   type User {
+    #Unique identifier for backend services
     _id: String!
+    #Good for email marketing campaigns
     email: String!
+    """
+      Multiline
+      description
+    """
     apiKey: String!
+    #First and last name together
     name: String!
+
+    swag: String @deprecated(reason: "No more yolo.")
   }
 
   type Query {
